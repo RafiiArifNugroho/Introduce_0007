ed3e7f49eec65701b0079d9340c9029fed383d02
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-04-28 11:16:46 +0700

    1

---
+++ class2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class persegi_panjang
+{
+    private;
+    int panjang;
+    int lebar;
+    int luas;
+
+public:
+    void input_data()
+    {
+        cout << "masukkan panjang: ";
+        cin >> panjang : cout << "masukkan lebar: ";
+        cin >> lebar;
+    }
+    void output_data()
+    {
+        cout << "tampilkan luas =" << luas << endl;
+    }
+    void hitung_luas()
+    {
+        luas = panjang * lebar;
+    }
+};
+int main()
+{
+    persegi_panjang pp;
+    pp.input_data();
+    pp.hitung_luas();
+    pp.output_data();
+};
