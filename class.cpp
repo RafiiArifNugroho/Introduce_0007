cec94e76a450ad18a83f83964ccd32319db72e8d
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-04-28 11:18:03 +0700

    2

--- class.cpp
+++ class.cpp
@@ -3,4 +3,17 @@ using namespace std;
 
 class mobil
 {
-}
+public:
+    string nama_mobil;
+    string tipe_mobil;
+
+private:
+    int input_data()
+    {
+        cout << "Masukkan nama mobil: ";
+        cin >> nama_mobil;
+        cout << "Masukkan tipe mobil: ";
+        cin >> tipe_mobil;
+        cout << "Masukkan harga mobil: ";
+        cin >> harga_mobil;
+    }
