7bd9afecc782f14296970d5b418c28cce062ca53
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-04-28 11:19:23 +0700

    3

--- class.cpp
+++ class.cpp
@@ -17,3 +17,25 @@ private:
         cout << "Masukkan harga mobil: ";
         cin >> harga_mobil;
     }
+    void output_data()
+    {
+        cout << "Harga mobil: = " << harga_mobil << endl;
+    }
+};
+int main()
+{
+
+    mobil rfiamar;
+    rfiamar.input_data();
+    cout << "Nama mobil: = " << rfiamar.nama_mobil << endl;
+    cout << "Tipe mobil: = " << rfiamar.tipe_mobil << endl;
+    rfiamar.output_data();
+};
+int main()
+{
+    mobil rfiamar;
+    rfiamar.intput_data();
+    cout << "Nama mobil: " << rfiamar.nama_mobil << endl;
+    cout << "Tipe mobil: " << rfiamar.tipe_mobil << endl;
+    rfiamar.output_data();
+}
